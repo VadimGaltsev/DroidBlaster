7d877b67a7ddb871388e431cc1d8f3769f0728f3
Vadim <vadim@MacBook-Pro-Vadim.local>
2018-07-11 18:44:09 +0300

    added new

---
+++ app/src/main/cpp/Resource.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vadim on 11.07.2018.
+//
+
+#include "Resource.h"
---
+++ app/src/main/cpp/Resource.h
@@ -0,0 +1,16 @@
+//
+// Created by Vadim on 11.07.2018.
+//
+
+#ifndef DROIDBLASTER_RESOURCE_H
+#define DROIDBLASTER_RESOURCE_H
+
+
+
+class Resource {
+
+};
+
+
+
+#endif //DROIDBLASTER_RESOURCE_H
